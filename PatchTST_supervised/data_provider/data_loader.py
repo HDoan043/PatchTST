465f3db0bff6b64b512213d2654f76a73e31ef05
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-09-06 11:15:20 +0700

    Update data_loader.py

--- PatchTST_supervised/data_provider/data_loader.py
+++ PatchTST_supervised/data_provider/data_loader.py
@@ -50,110 +50,7 @@ Chuẩn Dataset của repository:
 
     __invertransform__: không cần quan tâm
 '''
-
-class Time_Series_Practice_Dataset_00(Dataset):
-    def __init__(self, 
-                 root_path, 
-                 flag='train', 
-                 size=None,
-                 features='S', 
-                 data_path='train.csv',
-                 test_data_path ='test.csv',
-                 target='number_sold', 
-                 scale=True, 
-                 timeenc=0, 
-                 freq='d'):
-        # size [seq_len, label_len, pred_len]
-        # info
-        if size == None:
-            self.seq_len = 356 # Nhận 356 giá trị trong 1 năm trước
-            self.label_len = 60 # Dùng 60 giá trị trước để gợi ý cho decoder sinh
-            self.pred_len = 356 # Đoán 356 giá trị trong 1 năm trong tương lai
-        else:
-            self.seq_len = size[0]
-            self.label_len = size[1]
-            self.pred_len = size[2]
-        # init
-        assert flag in ['train', 'test', 'val']
-        type_map = {'train': 0, 'val': 1, 'test': 2}
-        self.set_type = type_map[flag]
-
-        self.features = features
-        self.target = target
-        self.scale = scale
-        self.timeenc = timeenc
-        self.freq = freq
-
-        self.root_path = root_path
-        self.train_data_path = data_path
-        self.test_data_path = test_data_path
-        self.__read_data__()
-
-    def __read_data__(self):
-        self.scaler = StandardScaler()
-        df_train_raw = pd.read_csv(os.path.join(self.root_path,
-                                          self.train_data_path))
-        df_test_raw = pd.read_csv(os.path.join(self.root_path, self.test_data_path))
-
-        train_data = df_train_raw[:int(len(df_train_raw)*0.7)]
-        vali_data = df_train_raw[int(len(df_train_raw)*0.7):]
-        label_data = df_train_raw[len(df_train_raw) - 365:]
-        test_data = pd.concat([label_data, df_test_raw], ignore_index = True)
-
-        set = [train_data, vali_data, test_data]
-        choosen_set = set[self.set_type]
-        
-        if self.features == 'M' or self.features == 'MS':
-            cols_data = choosen_set.columns[1:]
-            df_data = choosen_set[cols_data]
-        elif self.features == 'S':
-            df_data = choosen_set[[self.target]]
-
-        if self.scale:
-            train_data = df_data[:int(len(df_train_raw)*0.8)]
-            self.scaler.fit(train_data.values)
-            data = self.scaler.transform(df_data.values)
-        else:
-            data = df_data.values
-
-        df_stamp = choosen_set[['ds']]
-        df_stamp['ds'] = pd.to_datetime(df_stamp.ds)
-        if self.timeenc == 0:
-            df_stamp['month'] = df_stamp.ds.apply(lambda row: row.month, 1)
-            df_stamp['day'] = df_stamp.ds.apply(lambda row: row.day, 1)
-            df_stamp['weekday'] = df_stamp.ds.apply(lambda row: row.weekday(), 1)
-            df_stamp['hour'] = df_stamp.ds.apply(lambda row: row.hour, 1)
-            data_stamp = df_stamp.drop(['ds'], axis=1).values
-        elif self.timeenc == 1:
-            data_stamp = time_features(pd.to_datetime(df_stamp['ds'].values), freq=self.freq)
-            data_stamp = data_stamp.transpose(1, 0)
-
-        self.data_x = data
-        self.data_y = data
-        
-        self.data_stamp = data_stamp
-
-    def __getitem__(self, index):
-        s_begin = index
-        s_end = s_begin + self.seq_len
-        r_begin = s_end - self.label_len
-        r_end = r_begin + self.label_len + self.pred_len
-
-        seq_x = torch.tensor(self.data_x[s_begin:s_end], dtype=torch.float32)
-        seq_y = torch.tensor(self.data_y[r_begin:r_end], dtype=torch.float32)
-        seq_x_mark = torch.tensor(self.data_stamp[s_begin:s_end], dtype=torch.float32)
-        seq_y_mark = torch.tensor(self.data_stamp[r_begin:r_end], dtype=torch.float32)
-
-        return seq_x, seq_y, seq_x_mark, seq_y_mark
-
-    def __len__(self):
-        return len(self.data_x) - self.seq_len - self.pred_len + 1
-
-    def inverse_transform(self, data):
-        return self.scaler.inverse_transform(data)
-        
-
-class Dataset_ETT_hour(Dataset):
+ class Dataset_ETT_hour(Dataset):
     def __init__(self, root_path, flag='train', size=None,
                  features='S', data_path='ETTh1.csv',
                  target='OT', scale=True, timeenc=0, freq='h'):
@@ -333,14 +230,14 @@ class Dataset_ETT_minute(Dataset):
 
 class Dataset_Custom(Dataset):
     def __init__(self, root_path, flag='train', size=None,
-                 features='S', data_path='ETTh1.csv',
-                 target='OT', scale=True, timeenc=0, freq='h'):
+                 features='S', data_path='train.csv',
+                 target='number_sold', scale=True, timeenc=0, freq='d'):
         # size [seq_len, label_len, pred_len]
         # info
         if size == None:
-            self.seq_len = 24 * 4 * 4
-            self.label_len = 24 * 4
-            self.pred_len = 24 * 4
+            self.seq_len = 365    # Nhận vào dữ liệu 1 năm
+            self.label_len = 60    # Gợi ý cho decoder dữ liệu 2 tháng gần thời điểm dự đoán nhất
+            self.pred_len = 365    # Dự đoán dữ liệu của 1 năm tới
         else:
             self.seq_len = size[0]
             self.label_len = size[1]
@@ -366,16 +263,23 @@ class Dataset_Custom(Dataset):
                                           self.data_path))
 
         '''
+        Sắp xếp lại các cột theo thứ tự 'date', features, 'target'
         df_raw.columns: ['date', ...(other features), target feature]
         '''
         cols = list(df_raw.columns)
         cols.remove(self.target)
         cols.remove('date')
         df_raw = df_raw[['date'] + cols + [self.target]]
+        
         # print(cols)
+        '''
+        Chia train, validate và test ( train: 0.7, validate: 0.1, test: 0.2)
+        '''
+        # Xác định số lượng mỗi tập
         num_train = int(len(df_raw) * 0.7)
         num_test = int(len(df_raw) * 0.2)
         num_vali = len(df_raw) - num_train - num_test
+        # Xác định index đầu và cuối mỗi tập
         border1s = [0, num_train - self.seq_len, len(df_raw) - num_test - self.seq_len]
         border2s = [num_train, num_train + num_vali, len(df_raw)]
         border1 = border1s[self.set_type]
@@ -434,8 +338,8 @@ class Dataset_Custom(Dataset):
 
 class Dataset_Pred(Dataset):
     def __init__(self, root_path, flag='pred', size=None,
-                 features='S', data_path='ETTh1.csv',
-                 target='OT', scale=True, inverse=False, timeenc=0, freq='15min', cols=None):
+                 features='S', data_path='train.csv',
+                 target='number_sold', scale=True, inverse=False, timeenc=0, freq='d', cols=None):
         # size [seq_len, label_len, pred_len]
         # info
         if size == None:
