716e61ac4f35bba718e399d6d2a53f2bfeb16b9c
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-09-07 20:41:14 +0700

    Update pred_dataset.py

--- PatchTST_self_supervised/src/data/pred_dataset.py
+++ PatchTST_self_supervised/src/data/pred_dataset.py
@@ -200,10 +200,10 @@ class Dataset_ETT_minute(Dataset):
 
 class Dataset_Custom(Dataset):
     def __init__(self, root_path, split='train', size=None,
-                 features='S', data_path='ETTh1.csv',
-                 target='OT', scale=True, timeenc=0, freq='h',
+                 features='S', data_path='train.csv',
+                 target='number_sold', scale=True, timeenc=0, freq='d',
                  time_col_name='date', use_time_features=False, 
-                 train_split=0.7, test_split=0.2
+                 train_split=0.7, test_split=0.15
                  ):
         # size [seq_len, label_len, pred_len]
         # info
