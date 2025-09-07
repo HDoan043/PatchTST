5d85308f7daddf98f557364d9c19654ca19ae223
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-09-07 21:14:56 +0700

    Update patchtst_pretrain.py

--- PatchTST_self_supervised/patchtst_pretrain.py
+++ PatchTST_self_supervised/patchtst_pretrain.py
@@ -52,7 +52,7 @@ parser.add_argument('--model_type', type=str, default='based_model', help='for m
 args = parser.parse_args()
 print('args:', args)
 args.save_pretrained_model = 'patchtst_pretrained_cw'+str(args.context_points)+'_patch'+str(args.patch_len) + '_stride'+str(args.stride) + '_epochs-pretrain' + str(args.n_epochs_pretrain) + '_mask' + str(args.mask_ratio)  + '_model' + str(args.pretrained_model_id)
-args.save_path = 'saved_models/' + args.dset_pretrain + '/masked_patchtst/' + args.model_type + '/'
+args.save_path = '/kaggle/working/checkpoints/saved_models/' + args.dset_pretrain + '/masked_patchtst/' + args.model_type + '/'
 if not os.path.exists(args.save_path): os.makedirs(args.save_path)
 
 
