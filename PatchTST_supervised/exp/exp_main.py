63d76f863718a4e16e7c58c89a509e1e3f3d5976
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-09-20 19:11:17 +0700

    Update exp_main.py

--- PatchTST_supervised/exp/exp_main.py
+++ PatchTST_supervised/exp/exp_main.py
@@ -132,7 +132,7 @@ class Exp_Main(Exp_Basic):
                                             epochs = self.args.train_epochs,
                                             max_lr = self.args.learning_rate)
 
-        print("~~~~~ Numbers of trainable parameters: {} ~~~~~".format(sum(p.numel() for p in self.model.parameters() if p.require_grads)))
+        print("~~~~~ Numbers of trainable parameters: {} ~~~~~".format(sum(p.numel() for p in self.model.parameters() if p.requires_grad)))
 
         for epoch in range(self.args.train_epochs):
             iter_count = 0
