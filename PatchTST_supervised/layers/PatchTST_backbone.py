aa9eaa2d8acf0d89a4d1339b089c53f737409d16
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-09-29 17:27:58 +0700

    Update PatchTST_backbone.py

--- PatchTST_supervised/layers/PatchTST_backbone.py
+++ PatchTST_supervised/layers/PatchTST_backbone.py
@@ -211,7 +211,7 @@ class Reconstruct_Head(nn.Module):
     def forward(self, x):                                 # x: [bs x nvars x patch_num x seq_num x d_model]
         bs, nvars, pn, sn, d = x.shape
         x = torch.reshape(x, (bs*nvars*pn, sn, d ))       # x: [bs * nvars * patch_num x seq_num x d_model]
-        att = self.attention(x,x,x)                       # x: [bs * nvars * patch_num x seq_num x d_model]
+        att, _ = self.attention(x,x,x)                    # x: [bs * nvars * patch_num x seq_num x d_model]
         x = x + att                                       # x: [bs * nvars * patch_num x seq_num x d_model]
         x = self.ff(x)                                    # x: [bs * nvars * patch_num x seq_num x d_model]
         x = self.f1(x)                                    # x: [bs * nvars * patch_num x seq_num x d_model]
