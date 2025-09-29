e037a1565c832b7a4fc6f26f83396a726cb265c2
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-09-29 20:22:09 +0700

    Update run_longExp.py

--- PatchTST_supervised/run_longExp.py
+++ PatchTST_supervised/run_longExp.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
                         help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
     parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
     parser.add_argument('--train_size', type=int, defaut=100000, help = 'number of time stamps in train set')
-    parser.add_argument('--val_size', type = int, defautl = 10000, hel= = 'number of time stamps in val set')
+    parser.add_argument('--val_size', type = int, defautl = 10000, help = 'number of time stamps in val set')
 
     # forecasting task
     parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
