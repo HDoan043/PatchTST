7db7a0fa70c1650e3627cef565a60bb47f5f096c
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-09-29 20:26:32 +0700

    Update exp_main.py

--- PatchTST_supervised/exp/exp_main.py
+++ PatchTST_supervised/exp/exp_main.py
@@ -23,6 +23,7 @@ warnings.filterwarnings('ignore')
 class Exp_Main(Exp_Basic):
     def __init__(self, args):
         super(Exp_Main, self).__init__(args)
+        self.hybrid = args.hybrid
 
     def _build_model(self):
         model_dict = {
