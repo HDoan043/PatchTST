d7b7f627218e791c059e095c10bf8869e43a6fd9
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-10-06 00:08:07 +0700

    Update PatchTST_backbone.py

--- PatchTST_supervised/layers/PatchTST_backbone.py
+++ PatchTST_supervised/layers/PatchTST_backbone.py
@@ -209,7 +209,7 @@ class Combine_Channels(nn.Module):
         self.normalize = nn.LayerNorm(num_channels)
         self.attention = nn.MultiheadAttention(d_model, 8, batch_first = True)
         ls_ff = []
-        for _ in range(30):
+        for _ in range(15):
             ls_ff.extend(
                 [nn.Linear(num_channels, 1024), 
                 nn.ReLU(), 
