54c369390629a95e964a2c97b17ee1cef8a0adc3
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-10-05 23:50:55 +0700

    Update PatchTST_backbone.py

--- PatchTST_supervised/layers/PatchTST_backbone.py
+++ PatchTST_supervised/layers/PatchTST_backbone.py
@@ -238,7 +238,7 @@ class Combine_Channels(nn.Module):
             x = torch.reshape( x, (x.shape[0]*x.shape[1]*x.shape[2], x.shape[3], x.shape[4])) # x: [bs * seq_num * patch_num x nvars x d_model]
         else:
             x = torch.reshape( x, (x.shape[0]*x.shape[1], x.shape[2], x.shape[3]))  # x: [bs * patch_num x nvars x d_model]
-        att = self.attention(x,x,x)
+        att, _ = self.attention(x,x,x)
         x = att + x                                      # x: [bs * nvars * (seq_num x ) patch_num x d_model]
         x = torch.reshape(x, old_shape)                  # x: [bs x (seq_num x) patch_num x nvars x d_model]
         if len(x.shape) == 5:
