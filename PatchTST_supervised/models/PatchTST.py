82805567f6956f9f5425261557bfe57dc484c4f9
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-10-19 19:06:31 +0700

    Add bert

--- PatchTST_supervised/models/PatchTST.py
+++ PatchTST_supervised/models/PatchTST.py
@@ -40,11 +40,12 @@ class Model(nn.Module):
         multi_patches = configs.multi_patches
         hybrid = configs.hybrid
         self.hybrid = hybrid
-        
+        bert_ratio = configs.bert_ratio 
+                     
         revin = configs.revin
         affine = configs.affine
         subtract_last = configs.subtract_last
-        
+
         decomposition = configs.decomposition
         kernel_size = configs.kernel_size
         
@@ -61,7 +62,7 @@ class Model(nn.Module):
                                   attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm, store_attn=store_attn,
                                   pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout, padding_patch = padding_patch,
                                   pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
-                                  subtract_last=subtract_last, verbose=verbose, multi_patches = multi_patches, hybrid = hybrid, **kwargs)
+                                  subtract_last=subtract_last, verbose=verbose, multi_patches = multi_patches, hybrid = hybrid, bert_ratio = bert_ratio, **kwargs)
             self.model_res = PatchTST_backbone(c_in=c_in, context_window = context_window, target_window=target_window, patch_len=patch_len, stride=stride, 
                                   max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
                                   n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm, attn_dropout=attn_dropout,
@@ -69,7 +70,7 @@ class Model(nn.Module):
                                   attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm, store_attn=store_attn,
                                   pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout, padding_patch = padding_patch,
                                   pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
-                                  subtract_last=subtract_last, verbose=verbose, multi_patches = multi_patches, hybrid = hybrid, **kwargs)
+                                  subtract_last=subtract_last, verbose=verbose, multi_patches = multi_patches, hybrid = hybrid, bert_ratio = bert_ratio, **kwargs)
         else:
             self.model = PatchTST_backbone(c_in=c_in, context_window = context_window, target_window=target_window, patch_len=patch_len, stride=stride, 
                                   max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
@@ -78,7 +79,7 @@ class Model(nn.Module):
                                   attn_mask=attn_mask, res_attention=res_attention, pre_norm=pre_norm, store_attn=store_attn,
                                   pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout, padding_patch = padding_patch,
                                   pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
-                                  subtract_last=subtract_last, verbose=verbose, multi_patches = multi_patches, hybrid = hybrid, **kwargs)
+                                  subtract_last=subtract_last, verbose=verbose, multi_patches = multi_patches, hybrid = hybrid, bert_ratio = bert_ratio, **kwargs)
     
     
     def forward(self, x):           # x: [Batch, Input length, Channel]
