235f47fbef0d7836e7970ca5ea79cde3c8e200ce
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-10-20 00:04:17 +0700

    Update PatchTST_backbone.py

--- PatchTST_supervised/layers/PatchTST_backbone.py
+++ PatchTST_supervised/layers/PatchTST_backbone.py
@@ -354,7 +354,7 @@ class BertMask(nn.Module):
         
     def forward(self, x):                                        # x : [bs x nvars x patch_num x patch_len]
         shape = x.shape
-        mask = (torch.randn(shape) > self.bert_ratio, device = x.device).float()    # mask: [bs x nvars x patch_num x patch_len]
+        mask = (torch.randn(shape, device = x.device) > self.bert_ratio).float()    # mask: [bs x nvars x patch_num x patch_len]
         mask_x = x * mask                                        # mask_x: [bs x nvars x patch_num x patch_len]
 
         return mask_x, mask
